879391e4d7404f4dbb309424f0e16c7cc1d7681a
Michael Rowley <michaellrowley@protonmail.com>
2021-09-21 16:00:52 +0100

    Minor code-layout change.
    Moved structs to the top of the file.

--- src/main.c
+++ src/main.c
@@ -25,6 +25,28 @@
 #define NULL 0
 #endif
 
+typedef struct png_chunk {
+	fpos_t location; // Offset in file.
+	int32_t size; // Not including name or checksum.
+	unsigned char name [ 5 ]; // Don't print without '%.4s' to avoid over-reads/ID.
+	BYTE* data; // Allocated using calloc so free this when you're finished.
+	uint32_t checksum; // CRC32.
+	uint32_t real_checksum; // A CRC32 calculated by png-chunks.
+} chunk, *pchunk;
+
+typedef struct ihdr_data {
+// | <--4--> | <--4--> | <---1---> | <----1----> | <----1----> | <---1---> | <---1---> |
+// | width   | height  | bit-depth | colour-type | compression |   filter  | interlace |
+//      4    +    4    +     1     +      1      +      1      +     1     +     1     = 13 bytes.
+	int32_t width;
+	int32_t height;
+	BYTE bit_depth;
+	BYTE colour_type;
+	BYTE compression_type;
+	BYTE filter_type;
+	BYTE interlace_type;
+} ihdr_data, *pihdr_data;
+
 BOOL is_string_number( const char* string, size_t len ) {
 	for ( size_t i = 0; i < len; i++ ) {
 		if ( !isdigit( string[ i ] ) ) {
@@ -47,27 +69,6 @@ BOOL read_bytes( FILE* handle, size_t len, BYTE* buffer ) {
 	}
 	return TRUE;
 }
-
-typedef struct png_chunk {
-	fpos_t location; // Offset in file.
-	int32_t size; // Not including name or checksum.
-	unsigned char name [ 5 ]; // Don't print without '%.4s' to avoid over-reads/ID.
-	BYTE* data; // Allocated using calloc so free this when you're finished.
-	uint32_t checksum; // CRC32.
-	uint32_t real_checksum; // A CRC32 calculated by png-chunks.
-} chunk, *pchunk;
-typedef struct ihdr_data {
-// | <--4--> | <--4--> | <---1---> | <----1----> | <----1----> | <---1---> | <---1---> |
-// | width   | height  | bit-depth | colour-type | compression |   filter  | interlace |
-//      4    +    4    +     1     +      1      +      1      +     1     +     1     = 13 bytes.
-	int32_t width;
-	int32_t height;
-	BYTE bit_depth;
-	BYTE colour_type;
-	BYTE compression_type;
-	BYTE filter_type;
-	BYTE interlace_type;
-} ihdr_data, *pihdr_data;
 
 void chunk_crc32( chunk* chunk_ptr ) {
 	// Initial algorithm taken from: https://stackoverflow.com/a/21001712
